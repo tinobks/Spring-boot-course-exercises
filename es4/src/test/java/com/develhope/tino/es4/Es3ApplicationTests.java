0eb3549a8f2a525551d2a6a6e112c47af4f990c9
Tino <119343176+tinobks@users.noreply.github.com>
2024-01-23 18:53:51 +0100

    caricato es4

---
+++ es4/src/test/java/com/develhope/tino/es4/Es3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.tino.es4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
