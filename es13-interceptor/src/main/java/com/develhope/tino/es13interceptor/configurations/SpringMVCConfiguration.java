d2d5a8c2fa2fd4cc0c8900a6385b0b9d80ec08ea
Tino <119343176+tinobks@users.noreply.github.com>
2024-02-12 12:46:32 +0100

    Update SpringMVCConfiguration.java
    fix2 /legacy

--- es13-interceptor/src/main/java/com/develhope/tino/es13interceptor/configurations/SpringMVCConfiguration.java
+++ es13-interceptor/src/main/java/com/develhope/tino/es13interceptor/configurations/SpringMVCConfiguration.java
@@ -20,6 +20,6 @@ public class SpringMVCConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(apiLoggingInterceptor);
-        registry.addInterceptor(legacyInterceptor);
+        registry.addInterceptor(legacyInterceptor).addPathPatterns("/legacy");
     }
 }
